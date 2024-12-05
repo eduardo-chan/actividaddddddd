54b5addbd58f396d2000a789cd675830c73c098c
eduardo-chan <105666521+eduardo-chan@users.noreply.github.com>
2024-12-05 12:10:49 -0600

    Add files via upload

---
+++ gpds-ui/src/views/About.vue
@@ -0,0 +1,14 @@
+<!-- src/views/About.vue -->
+<template>
+    <div>
+      <h1>About</h1>
+      <p>Esta es la página de información actualización 4de dic 22s sadsd     s ss.</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'AboutPage'
+  }
+  </script>
+  
---
+++ gpds-ui/src/views/Home.vue
@@ -0,0 +1,14 @@
+<!-- src/views/Home.vue -->
+<template>
+    <div>
+      <h1>Home</h1>
+      <p>soy eduardooaaaaaOOO Bienvenido a la página de inicioooo.</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'HomePage'
+  }
+  </script>
+  
